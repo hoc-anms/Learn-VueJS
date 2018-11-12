10b3ad922c195dc302bb8d6db6c5ed6f1c38d66b
hoc-anm <phamvanhoc96@gmail.com>
2018-11-12 11:30:49 +0700

    add datacontact vue to vueclitest

---
+++ vueCliTest/src/DataContact.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <p>My address: {{address}}</p>
+        <hr>
+        <button @click="changePhone">Show My Phone</button>
+    </div>
+</template>
+
+<script>
+// xu ly data
+export default {
+    data: function(){
+        return {
+            address:"75 Ho tung mau",
+        }
+    },
+    methods:{
+        changePhone(){
+            this.address="0984125373";
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
+  
