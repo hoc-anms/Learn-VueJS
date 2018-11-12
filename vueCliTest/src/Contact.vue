044c1575ad7647dadf71586f278531cd06045caa
hoc-anm <phamvanhoc96@gmail.com>
2018-11-12 11:29:57 +0700

    add file contact vue in vueclitest

---
+++ vueCliTest/src/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- giao dien  -->
+        <h1>From contact. page</h1>
+        <datacontact-component></datacontact-component>
+    </div>
+</template>
+
+<script>
+// xu ly data
+import DataContact from './DataContact.vue'
+export default {
+    components:{
+        'datacontact-component':DataContact
+    },
+}
+
+</script>
+
+<style>
+
+</style>
+  
