bb9ee4d3efa7fb67ae48ec35b260f6faf2ab2dd5
hoc-anm <phamvanhoc96@gmail.com>
2018-11-12 11:24:16 +0700

    update app.vue vueCliTest

--- vueCliTest/src/App.vue
+++ vueCliTest/src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+     <!-- giao dien  -->
+    <h1>Hello world {{test}}</h1>
+    <contact-component></contact-component>
+  </div>
 </template>
 
 <script>
+// xu ly data
 export default {
+  data:function(){
+    return {test:'heljsffsjkjfhk  ',} 
+  },
+  methods:{
 
+  },
+  watch:{
+
+  },
+  computed:{
+
+  }
 }
+
 </script>
 
 <style>
