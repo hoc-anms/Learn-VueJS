cbdb3105c382fc287fba78c4509087ff7638a552
hoc-anm <phamvanhoc96@gmail.com>
2018-11-14 08:33:33 +0700

    folder src VueCliStructure

---
+++ VueCliStructure/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/shared/Header.vue';
+    import Footer from './components/shared/Footer.vue';
+    import Servers from './components/servers/Servers.vue';
+    import ServerDetails from './components/servers/ServerDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header,//app-header
+            Servers,//servers
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
